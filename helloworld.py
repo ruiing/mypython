c200d75b6114d927e99dbf73e0d2978e4d80fa94
ruiing <admin@mtz123.com>
2016-12-08 21:51:02 +0800

    ok1

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+print('hello world')
+input('you gev:')
+
+
+jaio = input('in put name:')
+temp = input('nhnjk：')
+a = True
+while a:
+    pass
+
+
+# nsajkngfk
---
+++ py2-1.py
@@ -0,0 +1,8 @@
+temp = input('猜猜我现在心里想的是神马：')
+guess = int(temp)
+if guess == 8:
+    print('猜中了！')
+    print('猜中了也木有奖励^_^')
+else:
+    print('猜错了……我现在心中想的是 8')
+print('游戏结束！')
